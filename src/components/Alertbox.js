3d12e068137f6db17820d544cfdb00aa4ce16eb6
Raj <raj350@mail.fresnostate.edu>
2021-04-07 07:37:39 -0700

    formatted alertbox

--- src/components/Alertbox.js
+++ src/components/Alertbox.js
@@ -1,17 +1,22 @@
 import React, {useState} from 'react';
 import '../css/Alertbox.css';
 import {BsFillXCircleFill} from 'react-icons/bs';
-
+import reactLogo from '../images/reactjs-icon.svg';
 
 const Alertbox = function({message}){
     const [show,showFunc] = useState(true);
+    console.log(reactLogo);
     return (
         <div className="alertbox" 
              style={show ? {display:"block"} : {display: "none"}}
              onClick={()=>showFunc(false)}
         >
-            <p className="close-icon"><BsFillXCircleFill/></p>
-            <p>{message}</p>
+            <div>
+                <img src={reactLogo} />
+                <p className="message">{message}</p>
+                <p className="close-icon"><BsFillXCircleFill/></p>
+            </div>
+  
         </div>
     );
 }
